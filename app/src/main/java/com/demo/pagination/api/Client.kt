2d5fd5ba142e8fda0dc41a7e49fae341db93ca53
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-18 16:07:34 -0600

    chore: add moshi configurations to retrofit client

--- app/src/main/java/com/demo/pagination/api/Client.kt
+++ app/src/main/java/com/demo/pagination/api/Client.kt
@@ -1,12 +1,16 @@
 package com.demo.pagination.api
 
+import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
-
-
+val moshi = Moshi.Builder()
+    .add(LocalDateAdapter)
+    .build()
 
 val client = Retrofit.Builder()
     .baseUrl("https://api.themoviedb.org/3")
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .build();
 
 //    https://api.themoviedb.org/3/movie/11?api_key=YOUR-API-KEY
