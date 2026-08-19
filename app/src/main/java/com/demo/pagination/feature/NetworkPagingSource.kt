f8baa5b339976462c2c0ec4f06f459242d396f72
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-18 20:49:05 -0600

    refactor: remove unnecessary param and switch to using page value class

--- app/src/main/java/com/demo/pagination/feature/NetworkPagingSource.kt
+++ app/src/main/java/com/demo/pagination/feature/NetworkPagingSource.kt
@@ -2,14 +2,12 @@ package com.demo.pagination.feature
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import com.demo.pagination.api.PagingTmdbResponse
 import com.demo.pagination.api.TmdbService
 import com.demo.pagination.api.TvShow
 
 class NetworkPagingSource(
-    val service: TmdbService,
-    val pageKey: Int
-): PagingSource<Int, TvShow>() {
+    val service: TmdbService
+): PagingSource<Page, TvShow>() {
     init {
         // the data source is expected to be immutable
         // invalidate PagingSource if data source
@@ -19,15 +17,15 @@ class NetworkPagingSource(
 //        }
     }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, TvShow> {
+    override suspend fun load(params: LoadParams<Page>): LoadResult<Page, TvShow> {
         try {
             // Start refresh at page 1 if undefined.
-            val nextPageNumber = params.key ?: 1
-            val response = service.getPopularShows(page = nextPageNumber)
+            val nextPageNumber = params.key ?: Page(number = 1)
+            val response = service.getPopularShows(page = nextPageNumber.number)
             return LoadResult.Page(
                 data = response.body()?.results?.toList() ?: emptyList(),
                 prevKey = null, // Only paging forward.
-                nextKey = nextPageNumber + 1
+                nextKey = Page(number = nextPageNumber.number + 1)
             )
         } catch (e: Exception) {
             // Handle errors in this block and return LoadResult.Error for
@@ -36,7 +34,7 @@ class NetworkPagingSource(
         }
     }
 
-    override fun getRefreshKey(state: PagingState<Int, TvShow>): Int? {
+    override fun getRefreshKey(state: PagingState<Page, TvShow>): Page? {
         // Try to find the page key of the closest page to anchorPosition from
         // either the prevKey or the nextKey; you need to handle nullability
         // here.
@@ -46,7 +44,14 @@ class NetworkPagingSource(
         //    initial page, so return null.
         return state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
-            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+
+
+            anchorPage?.prevKey?.let { prevPage ->
+                Page(number = prevPage.number + 1 )
+            }
+                ?: anchorPage?.nextKey?.let { nextPage ->
+                    Page(number = nextPage.number - 1)
+                }
         }
     }
 }
