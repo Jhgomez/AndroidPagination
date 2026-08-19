92b3a5d4d417326dfff6006458c47b18eca7e9ac
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-18 20:50:51 -0600

    chore: collect and consume pager

--- app/src/main/java/com/demo/pagination/MainActivity.kt
+++ app/src/main/java/com/demo/pagination/MainActivity.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.demo.pagination.api.TvShow
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemKey
 import com.demo.pagination.ui.theme.PaginationTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,9 +29,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val vm = viewModel<MainVm>()
-            LaunchedEffect(Unit) {
-                vm.getNextPage()
-            }
+            // this was just a test to confirm I was able to communicate with the backend
+//            LaunchedEffect(Unit) {
+//                vm.getNextPage()
+//            }
+
+            val shows = vm.tvShowsPagingFlow.collectAsLazyPagingItems()
 
             PaginationTheme {
                 LazyColumn(
@@ -38,10 +44,20 @@ class MainActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     items(
-                        vm.shows,
-                        TvShow::id
-                    ) {
-                        Text(it.toString())
+                        count = shows.itemCount,
+                        key = shows.itemKey { it.id }
+                    ) { index ->
+                        val show = shows[index]
+                        if (show != null) {
+                            Text(
+                                text = show.originalName,
+                                color = Color.White,
+                                modifier = Modifier.height(78.dp).background(Color.Blue),
+                                textAlign = TextAlign.Center
+                            )
+                        } else {
+                            Text("Placeholder")
+                        }
                     }
                 }
             }
