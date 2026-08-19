17294d7111b6bf89f93e740d500ff8e9ba863cc1
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-18 18:56:55 -0600

    chore: display tv shows in lazy list

--- app/src/main/java/com/demo/pagination/MainActivity.kt
+++ app/src/main/java/com/demo/pagination/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.demo.pagination.api.TvShow
 import com.demo.pagination.ui.theme.PaginationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +25,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val vm = viewModel<MainVm>()
+            LaunchedEffect(Unit) {
+                vm.getNextPage()
+            }
+
             PaginationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    items(
+                        vm.shows,
+                        TvShow::id
+                    ) {
+                        Text(it.toString())
+                    }
                 }
             }
         }
