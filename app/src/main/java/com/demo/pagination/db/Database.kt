41be7aae57eb143e75fd90c4a8ab0ef14795a7c6
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-19 18:31:37 -0600

    chore: add database

---
+++ app/src/main/java/com/demo/pagination/db/Database.kt
@@ -0,0 +1,20 @@
+package com.demo.pagination.db
+
+import android.content.Context
+import androidx.room3.ColumnTypeConverters
+import androidx.room3.Database
+import androidx.room3.Room
+import androidx.room3.RoomDatabase
+import androidx.sqlite.driver.AndroidSQLiteDriver
+
+@Database(
+    entities = [
+        TvShowEntity::class,
+        GenreReference::class,
+        OriginCountryReference::class
+    ],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun tvShowDao(): TvShowDao
+}
