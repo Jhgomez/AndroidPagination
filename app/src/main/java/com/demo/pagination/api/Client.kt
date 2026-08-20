a1ffbc33e1748e463c6d4419a1868ff4ea1280b6
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-19 20:06:50 -0600

    chore: add okhttp interceptor to retrofit client

--- app/src/main/java/com/demo/pagination/api/Client.kt
+++ app/src/main/java/com/demo/pagination/api/Client.kt
@@ -1,8 +1,18 @@
 package com.demo.pagination.api
 
 import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+private val logger = HttpLoggingInterceptor().apply {
+    setLevel(HttpLoggingInterceptor.Level.BODY)
+}
+private val baseClient =  OkHttpClient.Builder()
+.addInterceptor(logger)
+.build();
 
 val moshi = Moshi.Builder()
     .add(LocalDateAdapter)
@@ -11,6 +21,7 @@ val moshi = Moshi.Builder()
 val client = Retrofit.Builder()
     .baseUrl("https://api.themoviedb.org/3/")
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .client(baseClient)
     .build();
 
 //    https://api.themoviedb.org/3/movie/11?api_key=YOUR-API-KEY
