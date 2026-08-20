cfd6ccc82dab053f4edbe2db85b9d7803cc80e49
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-19 18:28:05 -0600

    refactor: change entities structure to one to many relantionships

--- app/src/main/java/com/demo/pagination/db/Entities.kt
+++ app/src/main/java/com/demo/pagination/db/Entities.kt
@@ -1,29 +1,29 @@
 package com.demo.pagination.db
 
 import androidx.room3.ColumnInfo
+import androidx.room3.ColumnTypeConverter
+import androidx.room3.Embedded
 import androidx.room3.Entity
 import androidx.room3.PrimaryKey
+import androidx.room3.Relation
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Entity
-data class TvShow(
+data class TvShowEntity(
     @PrimaryKey 
     val id: Int,
     val adult: Boolean,
     @ColumnInfo("backdrop_path") 
-    val backdropPath: String,
-    @ColumnInfo("genre_ids") 
-    val genreIds: Array<Int>,
-    @ColumnInfo("origin_country") 
-    val originCountry: Array<String>,
+    val backdropPath: String?,
     @ColumnInfo("original_language") 
     val originalLanguage: String,
     @ColumnInfo("original_name") 
     val originalName: String,
     val overview: String,
     val popularity: Float,
-    @ColumnInfo("poster_path") 
-    val posterPath: String,
+    @ColumnInfo("poster_path")
+    val posterPath: String?,
     @ColumnInfo("first_air_date") 
     val firstAirDate: LocalDate,
     val name: String,
@@ -31,46 +31,31 @@ data class TvShow(
     val voteAverage: Float,
     @ColumnInfo("vote_count") 
     val voteCount: Int
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+)
 
-        other as TvShow
+@Entity(primaryKeys = ["genreId", "showId"])
+data class GenreReference(
+    val genreId: Int,
+    val showId: Int
+)
 
-        if (adult != other.adult) return false
-        if (id != other.id) return false
-        if (popularity != other.popularity) return false
-        if (voteAverage != other.voteAverage) return false
-        if (voteCount != other.voteCount) return false
-        if (backdropPath != other.backdropPath) return false
-        if (!genreIds.contentEquals(other.genreIds)) return false
-        if (!originCountry.contentEquals(other.originCountry)) return false
-        if (originalLanguage != other.originalLanguage) return false
-        if (originalName != other.originalName) return false
-        if (overview != other.overview) return false
-        if (posterPath != other.posterPath) return false
-        if (firstAirDate != other.firstAirDate) return false
-        if (name != other.name) return false
+@Entity(primaryKeys = ["originCountryId", "showId"])
+data class OriginCountryReference(
+    val originCountryId: String,
+    val showId: Int
+)
 
-        return true
-    }
 
-    override fun hashCode(): Int {
-        var result = adult.hashCode()
-        result = 31 * result + id
-        result = 31 * result + popularity.hashCode()
-        result = 31 * result + voteAverage.hashCode()
-        result = 31 * result + voteCount
-        result = 31 * result + backdropPath.hashCode()
-        result = 31 * result + genreIds.contentHashCode()
-        result = 31 * result + originCountry.contentHashCode()
-        result = 31 * result + originalLanguage.hashCode()
-        result = 31 * result + originalName.hashCode()
-        result = 31 * result + overview.hashCode()
-        result = 31 * result + posterPath.hashCode()
-        result = 31 * result + firstAirDate.hashCode()
-        result = 31 * result + name.hashCode()
-        return result
-    }
-}
+data class TvShowQuery(
+    @Embedded val tvShow: TvShowEntity,
+    @Relation(
+        parentColumns = ["id"],
+        entityColumns = ["showId"]
+    )
+    val genreIds: Array<GenreReference>,
+    @Relation(
+        parentColumns = ["id"],
+        entityColumns = ["showId"]
+    )
+    val originCountry: Array<OriginCountryReference>
+)
