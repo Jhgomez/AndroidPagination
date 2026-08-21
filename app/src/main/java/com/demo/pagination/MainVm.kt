1cd1b66cbdd3fbb17edc2af361c7e5867664c6cd
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-21 11:44:40 -0600

    chore: pass mediator to pager and switch to db paging source

--- app/src/main/java/com/demo/pagination/MainVm.kt
+++ app/src/main/java/com/demo/pagination/MainVm.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
@@ -13,6 +14,7 @@ import com.demo.pagination.api.TvShow
 import com.demo.pagination.api.tmdbService
 import com.demo.pagination.db.TvShowQuery
 import com.demo.pagination.db.getAppDatabase
+import com.demo.pagination.feature.NetworkPagingMediator
 import com.demo.pagination.feature.NetworkPagingSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -36,19 +38,25 @@ class MainVm(application: Application): AndroidViewModel(application) {
 
     // see here https://developer.android.com/jetpack/androidx/releases/paging#3.5.0-alpha01
     // we have more flow options now, my current transitive dep is lower but I could use it
-    val tvShowsPagingFlow: Flow<PagingData<TvShow>> = Pager(
+    @OptIn(ExperimentalPagingApi::class)
+    val tvShowsPagingFlow: Flow<PagingData<TvShowQuery>> = Pager(
         // Configure how data is loaded by passing additional properties to
         // PagingConfig, such as pageSize and enabling or disabling placeholders.
         config = PagingConfig(
-            pageSize = 20, // TMBD defualt page size is 20 and that cant be change, this setting doesn't do anything n this case
+            pageSize = 20, // TMBD default page size is 20 and that cant be change, so we metter match it up here
             enablePlaceholders = true,
             prefetchDistance = 1,
-            initialLoadSize = 1
+            initialLoadSize = 1,
         ),
         pagingSourceFactory = {
-            source = NetworkPagingSource(service)
-            source
-        }
+//            source = NetworkPagingSource(service)
+//            source
+            db.tvShowDao().pagingSource()
+        },
+        remoteMediator = NetworkPagingMediator(
+            showsDb = db,
+            tmdbService = service
+        )
     )
         .flow
         .cachedIn(viewModelScope)
