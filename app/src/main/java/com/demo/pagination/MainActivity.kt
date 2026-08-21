78ef9bdacc22731e635902cef8e03025a54c3328
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-20 20:11:30 -0600

    chore: add visual loading

--- app/src/main/java/com/demo/pagination/MainActivity.kt
+++ app/src/main/java/com/demo/pagination/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,6 +25,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import com.demo.pagination.ui.theme.PaginationTheme
@@ -37,26 +44,45 @@ class MainActivity : ComponentActivity() {
             val shows = vm.tvShowsPagingFlow.collectAsLazyPagingItems()
 
             PaginationTheme {
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize(),
-                    contentPadding = PaddingValues(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    items(
-                        count = shows.itemCount,
-                        key = shows.itemKey { it.id }
-                    ) { index ->
-                        val show = shows[index]
-                        if (show != null) {
-                            Text(
-                                text = show.originalName,
-                                color = Color.White,
-                                modifier = Modifier.height(78.dp).background(Color.Blue),
-                                textAlign = TextAlign.Center
-                            )
-                        } else {
-                            Text("Placeholder")
+                Column(Modifier.fillMaxSize()) {
+                    Button({vm.invalidate()}) {
+                        Text("Invalidate")
+                    }
+
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        contentPadding = PaddingValues(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        items(
+                            count = shows.itemCount,
+                            key = shows.itemKey { it.id }
+                        ) { index ->
+                            val show = shows[index]
+                            if (show != null) {
+                                Text(
+                                    text = index.toString(),
+                                    color = Color.White,
+                                    modifier = Modifier.height(78.dp).background(Color.Blue),
+                                    textAlign = TextAlign.Center
+                                )
+                            } else {
+                                Text("Placeholder")
+                            }
+                        }
+
+                        if (
+                            shows.loadState.prepend == LoadState.Loading || // prepend not being called
+                            shows.loadState.append == LoadState.Loading ||
+                            shows.loadState.refresh == LoadState.Loading // refresh not being called
+                        ) {
+                            item {
+                                CircularProgressIndicator(
+                                    modifier =
+                                        Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally)
+                                )
+                            }
                         }
                     }
                 }
